d5c51547856442533de8c90206f8e73927407728
Shoeb Akhtar <malikshoeb924@gmail.com>
2026-09-07 23:36:33 +0530

    Add files via upload

---
+++ numpy_practice.py
@@ -0,0 +1,42 @@
+import numpy as np
+l = ([1,2,3],[4,5,6])
+arr = np.array(l)
+print (np.sum(arr, axis = 0))
+
+
+## mean:
+
+import numpy as np
+l = [3,4,6]
+arr = np. array ([l])
+print (np.mean (arr))
+
+
+#sum:
+
+import numpy as np
+
+l = [3,4,6]
+arr = np. array ([l])
+print (np.sum (arr))
+
+
+#min:
+
+
+import numpy as np
+
+l = [3,4,6]
+arr = np.array ([l])
+print (np.min (arr))
+
+
+
+#max:
+
+
+import numpy as np
+
+l = [3,4,7]
+arr = np.array ([l])
+print (np.max (arr))
---
+++ slicing.py
@@ -0,0 +1,45 @@
+import numpy as np
+s = [4,7,5]
+arr = np.array (s)
+print (arr[1])
+
+
+#2
+
+import numpy as np
+s = [6,9,4]
+arr = np.array (s)
+print (arr[0:2])
+
+
+
+#2D array
+
+import numpy as np
+g = [8,5,3],[6,8,3]
+arr = np.array(g)
+print (arr)
+
+
+
+
+#shape or size:
+
+
+import numpy as np
+l = ([1,2,3,],[4,5,6,])
+arr = np.array (l)
+print (arr.size)
+
+
+
+
+#Row Column:
+
+
+import numpy as np
+l = ([1,2,3,],[4,5,6,],)
+arr = np.array (l)
+print (arr.shape)
+
+
